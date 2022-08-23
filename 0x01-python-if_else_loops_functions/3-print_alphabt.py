19e5097d2f273c240ce4f781000a597108b5d8db
Huckiel <huckielsukai7@gmail.com>
2022-08-23 05:51:49 -0700

    Task 3 & 6 Updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-	# Author – Huckiel Sukai
-	for letter in range(97, 123):
-	    if chr(letter) != 'q' and chr(letter) != 'e':
-	        print("{}".format(chr(letter)), end="")
+# Author – Huckiel Sukai
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
